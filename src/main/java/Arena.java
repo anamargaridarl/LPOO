e056262be7666ec9a5aa0077cec17f015f0e355a
Ana Margarida <anamargaridarl@gmail.com>
2019-02-26 13:27:22 +0000

    added more details to game

--- src/main/java/Arena.java
+++ src/main/java/Arena.java
@@ -3,14 +3,36 @@ import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Arena {
 
     private int width;
     private int height;
+    private List<Wall> walls;
     Hero hero;
+
+
+    private List<Wall> createWalls() {
+        List<Wall> walls = new ArrayList<>();
+
+        for (int c = 0; c < width; c++) {
+            walls.add(new Wall(c, 0));
+            walls.add(new Wall(c, height - 1));
+        }
+
+        for (int r = 1; r < height - 1; r++) {
+            walls.add(new Wall(0, r));
+            walls.add(new Wall(width - 1, r));
+        }
+
+        return walls;
+    }
 
     public Arena(int width, int height)
     {
+        this.walls = createWalls();
         hero = new Hero();
         this.height = height;
         this.width = width;
@@ -20,6 +42,9 @@ public class Arena {
     {
         graphics.setBackgroundColor(TextColor.Factory.fromString("#48D1CC"));
         graphics.fillRectangle(new TerminalPosition(0, 0), new TerminalSize(width, height), ' ');
+
+        for (Wall wall : walls)
+            wall.draw(graphics);
 
         hero.draw(graphics);
     }
--- src/main/java/Wall.java
+++ src/main/java/Wall.java
@@ -1,2 +1,21 @@
+import com.googlecode.lanterna.SGR;
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+
 public class Wall {
+
+    Position position;
+
+    public Wall(int x, int y)
+    {
+        position = new Position(x,y);
+    }
+
+    public void draw(TextGraphics graphics)
+    {
+        graphics.setForegroundColor(TextColor.Factory.fromString("#8B4513"));
+        graphics.enableModifiers(SGR.BOLD);
+        graphics.putString(new TerminalPosition(position.getX(), position.getY()), "Z");
+    }
 }
